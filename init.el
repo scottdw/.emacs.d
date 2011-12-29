2aefae9a82bf9d66745b7e9d4147ce45bccf6d77
Scott Douglas-Watson <Scott.DouglasWatson@saga.co.uk>
2011-12-29 16:57:02 +0000

    Defaulting current language environment to UTF-8.

--- init.el
+++ init.el
@@ -74,3 +74,15 @@
       `((".*" ,user-temporary-file-directory t)))
 
 (load-file "~/.emacs.d/local.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(current-language-environment "UTF-8"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
