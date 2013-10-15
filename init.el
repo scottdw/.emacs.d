387a15cace59c418a84c680a93ddd164d8dd5afd
Scott Douglas-Watson <Scott.DouglasWatson@saga.co.uk>
2013-10-15 17:39:19 +0100

    Prefer custom-set-variables.

--- init.el
+++ init.el
@@ -23,18 +23,10 @@
   (edit-server-start)
   (setq edit-server-new-frame nil))
 
-(setq column-number-mode 1)
-(setq confirm-kill-emacs 'yes-or-no-p)
-(setq default-truncate-lines 1)
-(setq display-time-24hr-format t)
-(setq display-time-mode t)
-(setq european-calendar-style t)
-(setq inhibit-startup-message t)
-(setq make-backup-files nil)
-(setq show-paren-mode t)
-(setq show-paren-style 'parenthesis)
-(setq tab-always-indent 'complete)
-(setq visible-bell t)
+(add-hook 'calendar-load-hook
+          (lambda ()
+            (calendar-set-date-style 'european)))
+
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq initial-buffer-choice t)
@@ -97,8 +89,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(background-color "#002b36")
+ '(background-mode dark)
+ '(column-number-mode t)
+ '(confirm-kill-emacs (quote yes-or-no-p))
  '(current-language-environment "UTF-8")
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" default))))
+ '(cursor-color "#839496")
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "be7eadb2971d1057396c20e2eebaa08ec4bfd1efe9382c12917c6fe24352b7c1" default)))
+ '(display-time-24hr-format t)
+ '(display-time-mode t)
+ '(foreground-color "#839496")
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
+ '(show-paren-mode t)
+ '(tab-always-indent (quote complete))
+ '(truncate-lines t)
+ '(visible-bell t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
