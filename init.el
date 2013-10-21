bf9ab1749ab580da0e3c074ce1311362ce7a7669
Scott Douglas-Watson <Scott.DouglasWatson@saga.co.uk>
2013-10-21 13:19:43 +0100

    Try for org mode to use DejaVu fonts.

--- init.el
+++ init.el
@@ -116,7 +116,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit variable-pitch :foreground "#cb4b16" :height 1.3 :family "DejaVu"))))
+ '(variable-pitch ((t (:family "DejaVu")))))
 (put 'downcase-region 'disabled nil)
 (put 'ido-complete 'disabled nil)
 (put 'ido-exit-minibuffer 'disabled nil)
