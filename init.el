b72f95a4c036b2bde3d6c44be76f9ff3368b07d5
Scott Douglas-Watson <Scott.DouglasWatson@saga.co.uk>
2014-10-23 10:20:15 +0100

    Update melpa URL.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (eval-after-load "package"
   '(progn
      (add-to-list 'package-archives
-                  '("melpa" . "http://melpa.milkbox.net/packages/") t)))
+                  '("melpa" . "http://melpa.org/packages/") t)))
 
 (package-initialize)
 
