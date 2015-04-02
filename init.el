1582e4ab2c4979a09d486900bfcf67cab741c674
Scott Douglas-Watson <Scott.DouglasWatson@saga.co.uk>
2015-04-02 13:07:10 +0100

    Set solarized-use-more-italic to t
    This allows makes doc strings look different to strings in programming
    modes.

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 (setq solarized-distinct-fringe-background t)
 (setq solarized-use-variable-pitch nil)
 (setq solarized-scale-org-headlines nil)
+(setq solarized-use-more-italic t)
 (setq solarized-height-minus-1 1.0)
 (setq solarized-height-plus-1 1.0)
 (setq solarized-height-plus-2 1.0)
